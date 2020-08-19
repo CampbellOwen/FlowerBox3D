0f1199371c625872daa93ef9e25c3d5711dd75dc
Owen Campbell <ocampbell95@gmail.com>
2020-08-18 22:46:55 -0700

    Explody cubes!

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 // add styles
-import './style.css';
+import "./style.css";
 // three.js
-import * as THREE from 'three';
+import * as THREE from "three";
 
 // create the scene
 const scene = new THREE.Scene();
 
 // create the camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer();
+
+//var axesHelper = new THREE.AxesHelper(5);
+//scene.add( axesHelper );
 
 // set size
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // add canvas to dom
 document.body.appendChild(renderer.domElement);
-
-// add axis to the scene
-const axis = new THREE.AxesHelper(10);
-
-scene.add(axis);
 
 // add lights
 const light = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -29,21 +32,20 @@ light.position.set(100, 100, 100);
 
 scene.add(light);
 
-const light2 = new THREE.DirectionalLight(0xffffff, 1.0);
+// const light2 = new THREE.DirectionalLight(0xffffff, 1.0);
+//
+// light2.position.set(-100, 100, -100);
+//
+// scene.add(light2);
 
-light2.position.set(-100, 100, -100);
-
-scene.add(light2);
-
-const material = new THREE.MeshBasicMaterial({
-  color: 0xaaaaaa,
-  wireframe: true,
+const material = new THREE.PointsMaterial({
+  color: 0x00acab,
+  size: 0.2,
 });
 
 // create a box and add it to the scene
 const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
-
-scene.add(box);
+//scene.add(box);
 
 box.position.x = 0.5;
 box.rotation.y = 0.5;
@@ -52,6 +54,28 @@ camera.position.x = 5;
 camera.position.y = 5;
 camera.position.z = 5;
 
+const numSubdivisions = 2;
+let geometry = new THREE.Geometry();
+const scaleFactors = [];
+
+const di = 1 / (numSubdivisions + 1);
+for (let z = -0.5; z <= 0.5; z += di) {
+  for (let y = -0.5; y <= 0.5; y += di) {
+    for (let x = -0.5; x <= 0.5; x += di) {
+      const vertex = new THREE.Vector3(x, y, z)
+      geometry.vertices.push(vertex);
+      const distanceFromOrigin = vertex.distanceTo(new THREE.Vector3(0,0,0)) * 0.01;
+      scaleFactors.push(distanceFromOrigin);
+    }
+  }
+}
+
+const points = new THREE.Points(geometry, material);
+scene.add(points);
+console.log(points);
+
+console.log(geometry.vertices);
+
 camera.lookAt(scene.position);
 
 function animate(): void {
@@ -59,10 +83,22 @@ function animate(): void {
   render();
 }
 
+
 function render(): void {
   const timer = 0.002 * Date.now();
-  box.position.y = 0.5 + 0.5 * Math.sin(timer);
-  box.rotation.x += 0.1;
+  //box.position.y = 0.5 + 0.5 * Math.sin(timer);
+  //box.rotation.x += 0.1;
+  //points.geometry.vertices.forEach((vertex) => {
+  //  console.log(vertex);
+  //  vertex.add(0.1);
+  //  console.log(vertex);
+  //});
+  points.geometry.vertices.forEach((vertex, i) => {
+    vertex.x = vertex.x + scaleFactors[i] * (vertex.x < 0 ? -1 : 1);
+    vertex.y = vertex.y + scaleFactors[i] * (vertex.y < 0 ? -1 : 1);
+    vertex.z = vertex.z + scaleFactors[i] * (vertex.z < 0 ? -1 : 1);
+  });
+  points.geometry.verticesNeedUpdate = true;
   renderer.render(scene, camera);
 }
 
